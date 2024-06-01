b304da722a78ee889daff75fdb7e383077062c8e
이연승 <leeys951006@gmail.com>
2024-06-01 13:14:56 +0900

    중간 저장

--- app.js
+++ app.js
@@ -95,32 +95,32 @@ const server = http.createServer((req, res) => {
         //   }
         // });
 
-        // let c = `<!DOCTYPE html>
-        // <html lang="en">
-        // <head>
-        //   <meta charset="UTF-8">
-        //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //   <title>Work</title>
-        // </head>
-        // <body>
-        //   <form action="/submit" method="POST">
-        //     <label for="title">제목 :</label><br>
-        //     <input type="text" id="title" name="title"><br><br>
-        //     <label for="content">내용 :</label><br>
-        //     <textarea name="content" id="content"></textarea><br><br>
-        //     <input type="submit" value="submit">
-        //   </form>
+        let c = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Work</title>
+        </head>
+        <body>
+          <form action="/submit" method="POST">
+            <label for="title">제목 :</label><br>
+            <input type="text" id="title" name="title"><br><br>
+            <label for="content">내용 :</label><br>
+            <textarea name="content" id="content"></textarea><br><br>
+            <input type="submit" value="submit">
+          </form>
 
-        //   <h1>Stress 내역</h1>
+          <h1>Stress 내역</h1>
 
-        //   <ul id=list>
-        //     <li>${file}</li>
-        //   </ul>
+          <ul id=list>
+            <li>${file}</li>
+          </ul>
 
-        //   </script>
+          </script>
 
-        // </body>
-        // </html>`;
+        </body>
+        </html>`;
 
         fs.writeFile(`./data/${title}.html`, b, (err) => {
           if (err) {
@@ -131,6 +131,18 @@ const server = http.createServer((req, res) => {
           res.setHeader('Content-Type', 'text/html; charset=utf-8');
           res.end(b);
         });
+
+        fs.writeFile(`./data/work2.html`, c, (err) => {
+          if (err) {
+            console.log('Error');
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.end(c);
+        });
+
+
 
         // fs.writeFile('./public/work.html', c, (err) => {
         //   if (err) {
